14640224da0d5ca3c3492c88d2ce7fce3cbf4b8d
Christian Smith <18348535+RNVizion@users.noreply.github.com>
2026-07-01 10:27:59 -0400

    Delete fix.sh

--- fix.sh
+++
@@ -1,7 +0,0 @@
-cd .github/workflows
-sed -i \
-  -e 's#actions/checkout@v4#actions/checkout@v5#g' \
-  -e 's#actions/setup-python@v5#actions/setup-python@v6#g' \
-  -e 's#actions/upload-artifact@v4#actions/upload-artifact@v6#g' \
-  *.yml
-git add -A && git commit -m "Bump actions to Node 24 versions" && git push
